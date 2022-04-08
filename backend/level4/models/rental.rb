f40d7d86741ef4d8c6310a192d3d935aaa565314
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2022-04-08 19:18:47 +0200

    level4 complete

---
+++ backend/level4/models/rental.rb
@@ -0,0 +1,93 @@
+class Rental
+  attr_reader :id, :car, :start_date, :end_date, :distance
+
+  def initialize(attr)
+    @id = attr['id']
+    @car = $cars.find { |car| car.id == attr['car_id'] }
+    @start_date = Date.parse(attr['start_date'])
+    @end_date = Date.parse(attr['end_date'])
+    @distance = attr['distance']
+  end
+
+  def price_per_action_hash
+    {
+      id: id,
+      actions: actions_array
+    }
+  end
+
+  def duration
+    (end_date - start_date).to_i + 1
+  end
+
+  def distance_price
+    distance * car.price_per_km
+  end
+
+  def duration_price
+    price = 0
+    for i in 1..duration
+      case i
+      when 1
+        price += car.price_per_day
+      when 1..4
+        price += (car.price_per_day) * 0.9
+      when 5..10
+        price += (car.price_per_day) * 0.7
+      else
+        price += (car.price_per_day) * 0.5
+      end
+    end
+    price.to_i
+  end
+
+  def total_price
+    duration_price + distance_price
+  end
+
+  def commission
+    (total_price * 0.3).to_i
+  end
+
+  def insurance_fee
+    (commission * 0.5).to_i
+  end
+
+  def assistance_fee
+    duration * 100
+  end
+
+  def drivy_fee
+    commission - insurance_fee - assistance_fee
+  end
+
+  def actions_array
+    [
+      {
+        "who": "driver",
+        "type": "debit",
+        "amount": total_price
+      },
+      {
+        "who": "owner",
+        "type": "credit",
+        "amount": total_price - commission
+      },
+      {
+        "who": "insurance",
+        "type": "credit",
+        "amount": insurance_fee
+      },
+      {
+        "who": "assistance",
+        "type": "credit",
+        "amount": assistance_fee
+      },
+      {
+        "who": "drivy",
+        "type": "credit",
+        "amount": drivy_fee
+      }
+    ]
+  end
+end
